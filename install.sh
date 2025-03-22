316e6681dfcdf0751d2c8c2fb362058a1414ee83
yackrru <toshihiro.takushima@knowledgework.com>
2025-03-22 19:12:08 +0900

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -ex
 
 cd $(dirname $0)
 
 mkdir -p /tmp/yackrru-neovim-conf
+curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/refs/heads/main/installer.sh > /tmp/yackrru-neovim-conf/installer.sh
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/yackrru-neovim-conf/installer.sh
+mkdir -p ~/.cache/dein
 sh /tmp/yackrru-neovim-conf/installer.sh ~/.cache/dein
 
 rm -rf /tmp/yackrru-neovim-conf
--- local.vim
+++ local.vim
@@ -2,8 +2,8 @@ scriptencoding utf-8
 set encoding=utf-8
 
 " For system
-let g:python_host_prog = system('echo -n $(which python)')
-let g:python3_host_prog = system('echo -n $(which python)')
+let g:python_host_prog = system('echo -n $(which python3)')
+let g:python3_host_prog = system('echo -n $(which python3)')
 
 "-----------------------------------------------
 " Color settings
